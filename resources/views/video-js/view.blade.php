901cf930182ab5dcbd849eabf73d058c4c3ce66e
Sourov Biswas <sourovbuzz@gmail.com>
2023-03-30 16:05:28 +0600

    ok

--- resources/views/video-js/view.blade.php
+++ resources/views/video-js/view.blade.php
@@ -5,7 +5,28 @@
     <meta charset="utf-8">
     <title>videojs-vr Demo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/video.js@7.4.1/dist/video-js.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/videojs-vr@1.5.0/dist/videojs-vr.css">xd
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/videojs-vr@1.5.0/dist/videojs-vr.css">
+
+    <style type="text/css" media="all">
+        html,
+        body {
+            padding: 0;
+            margin: 0;
+            height: 100%
+        }
+
+        #videojs-vr-player {
+            width: 100% !important;
+            height: 100% !important;
+            overflow: hidden;
+            background-color: #fff
+        }
+
+        .phpdebugbar {
+            display: none;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -13,12 +34,7 @@
         crossorigin="anonymous">
         <source src="{{ asset('images/eagle-360.mp4') }}" type="video/mp4" crossorigin="anonymous">
     </video>
-    <ul>
-        <li><a href="test/debug.html">Run unit tests in browser.</a></li>
-        <li><a href="examples/cube.html">Cube Video example</a></li>
-        <li><a href="examples/fluid.html">"Fluid" video size example</a></li>
-        <li><a href="examples/iframe.html">Iframe example</a></li>
-    </ul>
+
     <script src="https://cdn.jsdelivr.net/npm/video.js@7.4.1/dist/video.js"
         integrity="sha256-vrHnet1gn0rfMfcNSTaMvp7bW1MU+hBXCuyPjfdxXhc=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/videojs-vr@1.5.0/dist/videojs-vr.js"
