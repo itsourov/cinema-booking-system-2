5cc64c64bd918223b733913627c5ae65d30b9f06
Sourov Biswas <sourovbuzz@gmail.com>
2023-03-31 21:53:26 +0600

    ok

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <script type='text/javascript'>
-        width = 720, height = 480, channel = 'livebdsourov', g = '1';
+        width = 720, height = 480, channel = 'livebdsourov2', g = '1';
     </script>
-    <script type='text/javascript' src='https://web.ucplayer1.com/static/scripts/hucaster.js'></script>
+    <script type='text/javascript' src='https://watch.webuctv.com/static/scripts/hucaster.js'></script>
 </x-app-layout>
